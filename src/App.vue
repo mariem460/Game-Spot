76a57772b6a6f781d5f289c8ac9e9a66888eec2f
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-10-20 14:19:32 +0200

    carousel

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <homeComp></homeComp>
+  <CarouselComp></CarouselComp>
   <router-view></router-view>
   
 </template>
 
 <script>
   import homeComp from "./components/Home/homeComp.vue";
+import CarouselComp from "./components/utils/carouselComp.vue";
 
 
  export default {
   components: {
-    homeComp
-  }
+    homeComp,
+    CarouselComp
+}
 
  }
 
---
+++ src/components/utils/carouselComp.vue
@@ -0,0 +1,53 @@
+<template>
+    <carousel class="P_top"   :items-to-show="1"
+    :wrap-around="true">
+    <slide v-for="slide in slides" :key=slide.id >
+       <div class="slider_bck" style="background: url(http://placebeard.it/1080/720);">
+        <div class="slide_content">
+            <div class="item">
+                <div>
+                    Title of the article
+                </div>
+                <div>
+                    There are many variations of passages of Lorem Ipsum available,
+                     but the majority have suffered alteration in some form, by injected humour.
+                </div>
+
+            </div>
+        </div>
+
+       </div>
+    </slide>
+   
+    <template #addons>
+        <pagination />
+
+    </template>
+      
+
+    </carousel>
+
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
+
+   export default {
+    components:{
+        Carousel,
+        Slide,
+        Pagination
+    },
+    data(){
+        return {
+            slides: [
+                {id: 1}, 
+                {id:2},
+                {id:3}]
+        }
+    }
+
+   }
+
+</script>
